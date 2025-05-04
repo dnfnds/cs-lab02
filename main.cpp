4b59dca0cddd74e2d3820c07381e73d6968e3a81
bob (IvanovArtAl) <IvanovArtAl@mpei.ru>
2025-05-04 16:42:53 +0300

    Добавил вывод произведения чисел

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'
+    cout << "A + B = " << a + b << '\n';
          << "A - B = " << a - b << '\n';
+         << "A * B = " << a * b << '\n';
 
     return 0;
 }
